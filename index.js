e166529109be7a3e091b6ec67636e8de23d824f4
Xiaozhe Yao <askxzyao@gmail.com>
2021-06-07 18:01:10 +0200

    minor fixes

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 
+async function main() {
+    const isEdge = core.getInput('edge');
+    console.log(isEdge)
+    if (eval(isEdge)) {
+        console.info('Downloading Latest AID (Edge version)')
+        await exec.exec('curl https://releases.autoai.org/aid/install.sh | sudo bash -s -- edge')
+    } else {
+        console.info('Downloading Latest AID (Stable version)')
+        await exec.exec('curl https://releases.autoai.org/aid/install.sh | sudo bash -s')
+    }
+    
+    await exec.exec('ls')
+}
 try {
-    const reporting = core.getInput('reporting')
+    main()
 } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
 }
