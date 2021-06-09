d59d2f707a816eb70c96bdfda75091f644bc4ab8
Xiaozhe Yao <askxzyao@gmail.com>
2021-06-09 17:46:20 +0200

    update the actions with default settings

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ const exec = require('@actions/exec');
 
 async function main() {
     const isEdge = core.getInput('edge');
-    console.log(isEdge)
+    console.log(eval(isEdge))
+    await exec.exec('curl -s https://releases.autoai.org/aid/install.sh -o install.sh')
+    await exec.exec('sudo chmod +x install.sh')
     if (eval(isEdge)) {
-        console.info('Downloading Latest AID (Edge version)')
-        await exec.exec('curl -s https://releases.autoai.org/aid/install.sh | sudo bash -s -- edge')
+        console.info('Installing Latest AID (Edge version)')
+        await exec.exec('sudo bash ./install.sh edge')
     } else {
-        console.info('Downloading Latest AID (Stable version)')
-        await exec.exec('curl -s https://releases.autoai.org/aid/install.sh | sudo bash -s')
+        console.info('Installing Latest AID (Stable version)')
+        await exec.exec('sudo bash ./install.sh')
     }
-    
-    await exec.exec('ls')
-}
+    await exec.exec('aid --version')
+}   
 try {
     main()
 } catch (error) {
